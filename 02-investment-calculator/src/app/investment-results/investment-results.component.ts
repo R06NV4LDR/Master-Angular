b37bea4dc265c476974224a711bbafda42d0a9f0
Ronny B Good <ronny.bruhin+bbc@hotmail.com>
2025-08-15 16:29:45 +0200

    investment calculator: bug fixing

--- 02-investment-calculator/src/app/investment-results/investment-results.component.ts
+++ 02-investment-calculator/src/app/investment-results/investment-results.component.ts
@@ -1,12 +1,22 @@
-import { Component } from '@angular/core';
+import { CurrencyPipe } from "@angular/common";
+import { Component, input } from "@angular/core";
 
 @Component({
-  selector: 'app-investment-results',
+  selector: "app-investment-results",
   standalone: true,
-  imports: [],
-  templateUrl: './investment-results.component.html',
-  styleUrl: './investment-results.component.css'
+  imports: [CurrencyPipe],
+  templateUrl: "./investment-results.component.html",
+  styleUrl: "./investment-results.component.css",
 })
 export class InvestmentResultsComponent {
-
+  results = input<
+      {
+        year: number;
+        interest: number;
+        valueEndOfYear: number;
+        annualInvestment: number;
+        totalInterest: number;
+        totalAmountInvested: number;
+      }[]
+    >();
 }
